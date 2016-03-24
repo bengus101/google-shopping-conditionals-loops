c2f10e10614d4216b1214b1e6f048186af7d793d
bengus101 <bgustafson29@yahoo.com>
2016-03-24 08:27:10 -0700

    Problem 2: 1 item on backorder

--- google_shopping.js
+++ google_shopping.js
@@ -3,8 +3,7 @@ var data = require("./products.json")
 var count = 0; 
 
 for (var i = 0; i <data['items'].length; i++) {
-	if (data['items'][i].kind === 'shopping#product') {
-		count++;
+	if (data.['i'].items.product.inventories[0].availability === 'backorder') {
+	console.log(data.items[i].product.title);	
 	}
 }
-console.log(count);
